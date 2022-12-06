249f9aa6fbe8ee1ff547a5870a78fb16c6b4cb9e
edwin <ngisahedwin@gmail.com>
2022-12-06 20:23:07 +0300

    implemented whiteboard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import LoginPage from "./components/Login/login";
 import RegisterPage from './components/Register/Register';
+import WhiteBoard from './components/Whiteboard'
 
 function App() {
   return (
     <div className="App">
       {/* <LoginPage /> */}
       <RegisterPage />
+      <WhiteBoard/>
     </div>
   );
 }
