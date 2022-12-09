9bc63e49ddd6abf8687b41dcb46413d21975c889
Daniel Ngoya <46535202+BlackHawk0@users.noreply.github.com>
2022-12-09 21:56:43 +0300

    Dashboard (#8)
    * assessemnt component
    
    * dahsboard
    
    * user profile
    
    * Routes
    
    * routing
    
    * render components
    
    * updated modules
    
    * creating an assesement
    
    * render an assessment
    
    * implemented whiteboard
    
    * Remove import registerpage
    
    * Edit app.js
    
    * assessement
    
    * enable user to be able to register if not registered
    
    * enable user to login if registered
    
    * corrected the register page router
    
    * register page and assessment routes
    
    * enable user to login if registered
    
    * Add timer component
    
    * added api call
    
    * added api call
    
    * routes
    
    * invitation form
    
    * fix logic issues
    
    * "init" (#5)
    
    Co-authored-by: Daniel Ngoya <46535202+BlackHawk0@users.noreply.github.com>
    
    * added statistics to TM dashboard
    
    * register redirect
    
    * added bootstrap
    
    * render TM dashboard
    
    * invitation success toast
    
    * minor fixes on indentation
    
    * added api url
    
    Co-authored-by: edwin <ngisahedwin@gmail.com>
    Co-authored-by: Stephen <skairu09@gmail.com>
    Co-authored-by: StephenKairu <108527087+StephenKairu@users.noreply.github.com>
    Co-authored-by: Ramayan0 <86400895+Ramayan0@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import LoginPage from "./components/Login/login";
+
+import RegisterPage from "./components/Register/Register";
+// import { ReviewCard } from './components/Reviews/ReviewCard';
+// import { ReviewForm } from './components/Reviews/ReviewForm';
+import Reviews from "./components/Reviews/reviews";
+import StudentList from "./components/StudentList/StudentList";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import FeedBack from "./components/FeedBack/FeedBack";
+import CreateAssessment from "./components/Assement/CreateAssement";
+import InvitationForm from "./components/Assement/InvitationForm";
+import { Route, Routes } from "react-router-dom";
+import Timer from "./components/Timer/timer";
+import MainPage from "./Admin/MainPage";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {/* <UserProfile /> */}
+
+      {/* <Dashboard /> */}
+      {/* <Index /> */}
+      {/* <Timer /> */}
+      {/* <LoginPage /> */}
+      {/* <RegisterPage /> */}
+      {/* <ReviewCard />
+      <ReviewForm /> */}
+      {/* <Reviews /> */}
+
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/dashboard" element={<MainPage />} />
+        <Route exact path="/studentList" element={<StudentList />} />
+        <Route exact path="/feedBack" element={<FeedBack />} />
+        <Route exact path="/createAssessment" element={<CreateAssessment />} />
+        <Route exact path="/invitation" element={<InvitationForm />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
