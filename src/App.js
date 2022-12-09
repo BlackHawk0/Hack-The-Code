55bb859e5e4d3f6b1531e5c720903f5fe177fede
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-09 16:16:08 +0300

    render TM dashboard

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import RegisterPage from './components/Register/Register';
 // import { ReviewForm } from './components/Reviews/ReviewForm';
 import Reviews from "./components/Reviews/reviews";
 import StudentList from "./components/StudentList/StudentList";
-import FeedBack from "./components/FeedBack/FeedBack";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import FeedBack from "./components/FeedBack/FeedBack"
 import CreateAssessment from "./components/Assement/CreateAssement";
 import InvitationForm from "./components/Assement/InvitationForm";
 import { Route, Routes } from "react-router-dom";
 import Timer from "./components/Timer/timer";
+import MainPage from "./Admin/MainPage";
 
 function App() {
   return (
@@ -21,18 +22,20 @@ function App() {
 
       {/* <Dashboard /> */}
       {/* <Index /> */}
-      <Timer />
+      {/* <Timer /> */}
       {/* <LoginPage /> */}
-      <RegisterPage />
+      {/* <RegisterPage /> */}
       {/* <ReviewCard />
       <ReviewForm /> */}
       {/* <Reviews /> */}
+      
 
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/dashboard" element={<MainPage />} />
         <Route exact path="/studentList" element={<StudentList />} />
         <Route exact path="/feedBack" element={<FeedBack />} />
         <Route exact path="/createAssessment" element={<CreateAssessment />} />
