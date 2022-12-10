8a2db7cb96153c55c484e6f81996bb3dcfc549bd
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-10 10:47:03 +0300

    linked updated routes

--- src/Student/StudentMainPage.jsx
+++ src/Student/StudentMainPage.jsx
@@ -3,7 +3,10 @@ import Navbar from './Navbar';
 import NavPage from './NavPage';
 import Sidebar from './Sidebar';
 import './App2.css'
-function StudentMainPage() {
+function StudentMainPage({user}) {
+    if (user) {
+      return <h1>Welcome, {user.username}!</h1>;
+    }
   return (
     <>
       {/* navbar section */}
